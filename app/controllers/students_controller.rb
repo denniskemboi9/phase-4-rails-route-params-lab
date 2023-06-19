b0d150056b4b55423a2c58749ce0685bb8050e87
DennisKemboi <cherogonydennis9@gmail.com>
2023-06-19 10:55:01 +0300

    Completed Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,8 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student_by_id = Student.find(params[:id])
+    render json: student_by_id
+  end
 end
